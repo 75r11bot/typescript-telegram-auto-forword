859dedeb136922c89f1b004e0407c11698ef8baa
nus3206 <nus32063206@gmail.com>
2024-05-26 15:05:30 +0700

    add

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import dotenv from "dotenv";
 import axios from "axios";
+import axiosRetry from "axios-retry";
 import express, { Request, Response } from "express";
 import { TelegramClient } from "telegram";
 import { StringSession } from "telegram/sessions";
@@ -10,6 +11,7 @@ import { processBonusCode, responseResult } from "./services";
 
 dotenv.config();
 
+// Environment Variables
 const apiId = Number(process.env.API_ID);
 const apiHash = process.env.API_HASH || "";
 const sourceChannelId = Number(process.env.SOURCE_CHANNEL_ID);
@@ -18,57 +20,71 @@ const phoneNumber = process.env.APP_YOUR_PHONE || "";
 const userPassword = process.env.APP_YOUR_PWD || "";
 const telegramChannelId = Number(process.env.TELEGRAM_CHANNEL_ID);
 const port = Number(process.env.PORT) || 5000;
+const reconnectInterval = 5000; // Reconnect interval in milliseconds
 
 const apiEndpoints: string[] = [];
 
+// Ensure sessions directory exists
 const sessionsDirectory = "./sessions";
-
 if (!fs.existsSync(sessionsDirectory)) {
   fs.mkdirSync(sessionsDirectory);
 }
 
+// Load or create session file
 const sessionFilePath = "./sessions/session.txt";
 const sessionString = fs.existsSync(sessionFilePath)
   ? fs.readFileSync(sessionFilePath, "utf-8")
   : "";
 
+// Initialize Telegram Client
 const client = new TelegramClient(
   new StringSession(sessionString),
   apiId,
   apiHash,
   {
     connectionRetries: 10,
-    timeout: 86400000, // Set timeout to a large number (e.g., 24 hours)
+    timeout: 86400000, // 24 hours
     useWSS: true,
   }
 );
 
-async function get_input(prompt: string): Promise<string> {
+// Axios retry configuration
+axiosRetry(axios, {
+  retries: 3,
+  retryDelay: (retryCount) => {
+    return retryCount * 1000; // time interval between retries
+  },
+  retryCondition: (error) => {
+    if (
+      error.code === "ECONNABORTED" ||
+      (error.response && error.response.status >= 500)
+    ) {
+      return true;
+    }
+    return false; // Ensure the function returns a boolean
+  },
+});
+
+// Function to get user input
+async function getInput(prompt: string): Promise<string> {
   return new Promise((resolve) => {
     process.stdout.write(prompt);
     process.stdin.once("data", (data) => resolve(data.toString().trim()));
   });
 }
 
+// Function to get login code from Telegram messages
 async function getLoginCode(): Promise<string> {
-  return new Promise<string>(async (resolve, reject) => {
-    const timeout = setTimeout(() => {
-      reject(new Error("Timeout"));
-    }, 60000); // Timeout after 60 seconds
+  return new Promise<string>((resolve, reject) => {
+    const timeout = setTimeout(() => reject(new Error("Timeout")), 60000);
 
     const handler = async (event: any) => {
       const message = event.message;
-      console.log("Received message for login code:", message.message);
-
-      if (
-        message.peerId &&
-        message.peerId.channelId &&
-        message.peerId.channelId.equals(telegramChannelId)
-      ) {
+      if (message.peerId?.channelId?.equals(telegramChannelId)) {
         const match = message.message.match(/(\d{5,6})/);
         if (match) {
-          clearTimeout(timeout); // Clear the timeout
-          client.removeEventHandler(handler, new NewMessage({})); // Remove the handler
+          clearTimeout(timeout);
+          client.removeEventHandler(handler, new NewMessage({}));
           resolve(match[1]);
         }
       }
@@ -77,19 +93,20 @@ async function getLoginCode(): Promise<string> {
     client.addEventHandler(handler, new NewMessage({}));
   }).catch(async (error) => {
     console.error("Error getting login code:", error);
-    return await get_input("Enter the code: ");
+    return await getInput("Enter the code: ");
   });
 }
 
+// Function to list all chats
 async function listChats() {
   await startClient();
-
   const dialogs = await client.getDialogs();
-  for (const dialog of dialogs) {
-    console.log(`Chat ID: ${dialog.id}, Title: ${dialog.title}`);
-  }
+  dialogs.forEach((dialog) =>
+    console.log(`Chat ID: ${dialog.id}, Title: ${dialog.title}`)
+  );
 }
 
+// Function to forward new messages
 async function forwardNewMessages() {
   console.log("Process subscribed message to auto forward successfully");
   await startClient();
@@ -105,74 +122,66 @@ async function forwardNewMessages() {
     try {
       const sourceEntity = await client.getEntity(sourceChannelId);
       const destinationEntity = await client.getEntity(destinationChannelId);
-
-      console.log("Source Entity:", sourceEntity);
-      console.log("Destination Entity:", destinationEntity);
-
       const message = event.message;
       const peerId = message.peerId;
-      const channelId = peerId && peerId.channelId;
 
-      if (channelId && channelId.equals(sourceEntity.id)) {
-        console.log(
-          `Forwarding message with ID ${message.id} from ${sourceChannelId} to ${destinationChannelId}`
-        );
-
+      if (peerId?.channelId?.equals(sourceEntity.id)) {
+        await processBonusCode(apiEndpoints, message.message);
         await client.forwardMessages(destinationEntity, {
           fromPeer: sourceEntity,
           messages: [message.id],
         });
-
         console.log(
           `Message forwarded from ${sourceChannelId} to ${destinationChannelId}`
         );
-
-        await processBonusCode(apiEndpoints, message.message);
-        console.log("processBonusCode called successfully");
       } else {
         console.log(
-          "New Message received from the source channel, cannot forward it to the destination channel"
+          "New message received from the source channel, cannot forward it to the destination channel"
         );
       }
     } catch (error: any) {
-      console.error("Error handling new message event:", error);
-
-      if (error.message.includes("FloodWait")) {
-        console.error(
-          "FloodWait error: Too many requests in a short period. Try again later."
-        );
-      } else if (
-        error.message.includes("ChatWriteForbidden") ||
-        error.message.includes("ChatForbidden")
-      ) {
-        console.error(
-          "ChatWriteForbidden error: Bot or user does not have permission to write to the destination channel"
-        );
-      } else if (
-        error.message.includes("MessageNotModified") ||
-        error.message.includes("WebPageNotModified")
-      ) {
-        console.error(
-          "MessageNotModified error: Message content has not been modified"
-        );
-      } else {
-        console.error("Unexpected error:", error);
-      }
+      handleForwardingError(error);
     }
   }, new NewMessage({}));
 }
 
+// Error handling for message forwarding
+function handleForwardingError(error: any) {
+  console.error("Error handling new message event:", error);
+  if (error.message.includes("FloodWait")) {
+    console.error(
+      "FloodWait error: Too many requests in a short period. Try again later."
+    );
+  } else if (
+    error.message.includes("ChatWriteForbidden") ||
+    error.message.includes("ChatForbidden")
+  ) {
+    console.error(
+      "ChatWriteForbidden error: Bot or user does not have permission to write to the destination channel"
+    );
+  } else if (
+    error.message.includes("MessageNotModified") ||
+    error.message.includes("WebPageNotModified")
+  ) {
+    console.error(
+      "MessageNotModified error: Message content has not been modified"
+    );
+  } else {
+    console.error("Unexpected error:", error);
+  }
+}
+
+// Function to ping endpoints
 async function pingEndpoints() {
   const endpoints = [
     process.env.API_ENDPOINT_1,
-    process.env.API_ENDPOINT_2,
-    process.env.API_ENDPOINT_3,
+    // Add more endpoints as needed
   ].filter(Boolean) as string[];
 
   const siteId = "1451470260579512322";
   const siteCode = "ybaxcf-4";
   const platformType = "2";
-  const token = process.env.H25_TOKEN1; // Replace YOUR_TOKEN_HERE with the actual token
+  const token = process.env.H25_TOKEN1; // Replace with actual token
 
   const headers = {
     token: token,
@@ -183,13 +192,11 @@ async function pingEndpoints() {
     try {
       const url = `${endpoint}/v/user/refreshUserFund?siteId=${siteId}&siteCode=${siteCode}&platformType=${platformType}`;
       const response = await axios.get(url, { headers });
-
       if (response.status === 200) {
         apiEndpoints.push(endpoint);
         if (response.data.code === 10000) {
           console.log(`Token ${token} is ready.`);
         } else if (response.data.code === 10140) {
-          const token = process.env.H25_TOKEN2; // Replace YOUR_TOKEN_HERE with the actual token
           console.log(`Token ${token} is expired.`);
         }
       } else {
@@ -197,28 +204,20 @@ async function pingEndpoints() {
           `Endpoint ${endpoint} is not reachable. Status code: ${response.status}`
         );
       }
-    } catch (error) {
+    } catch (error: any) {
       console.error(`Error connecting to ${endpoint}: ${error}`);
     }
   }
 }
 
+// Function to start Telegram client
 async function startClient() {
   try {
     await client.start({
       phoneNumber: async () => phoneNumber,
       password: async () => userPassword,
       phoneCode: async () => await getLoginCode(),
-      onError: (err: Error) => {
-        if (err.message.includes("AUTH_KEY_DUPLICATED")) {
-          console.log(
-            "AUTH_KEY_DUPLICATED error detected. Regenerating session..."
-          );
-          regenerateSession();
-        } else {
-          console.log("Client start error:", err);
-        }
-      },
+      onError: handleClientError,
     });
     const me = (await client.getEntity("me")) as Api.User;
     const displayName = [me.firstName, me.lastName].filter(Boolean).join(" ");
@@ -229,6 +228,17 @@ async function startClient() {
   }
 }
 
+// Handle errors during client start
+function handleClientError(err: Error) {
+  if (err.message.includes("AUTH_KEY_DUPLICATED")) {
+    console.log("AUTH_KEY_DUPLICATED error detected. Regenerating session...");
+    regenerateSession();
+  } else {
+    console.log("Client start error:", err);
+  }
+}
+
+// Regenerate session on AUTH_KEY_DUPLICATED error
 async function regenerateSession() {
   try {
     console.log("Regenerating session...");
@@ -239,24 +249,40 @@ async function regenerateSession() {
   }
 }
 
+// Reconnection logic
+function startReconnect() {
+  console.log("[Started reconnecting]");
+  setTimeout(async () => {
+    try {
+      await startClient();
+    } catch (error) {
+      console.error("Failed to reconnect:", error);
+      startReconnect();
+    }
+  }, reconnectInterval);
+}
+
+// Main function to start the bot
 async function main() {
   await pingEndpoints();
   console.log(`======= Serving on http://0.0.0.0:${port}/ ======`);
   await listChats();
   await forwardNewMessages();
+  startReconnect();
 }
 
+// Express setup
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`
     <html>
       <body>
-        <h1>โค้ดโบนัส H25 Response</h1>
+        <h1>Bonus Code H25 Response</h1>
         <pre>${JSON.stringify(responseResult, null, 2)}</pre>
       </body>
     </html>
-  `); // Display responseResult in a formatted way
+  `);
 });
 
 app.listen(port, () => {
